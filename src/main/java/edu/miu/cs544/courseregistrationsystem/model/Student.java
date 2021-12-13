6f33100968096cb1f35e007f37bea17c86b7a8da
Girmay-1 <54249189+Girmay-1@users.noreply.github.com>
2021-12-13 13:39:45 -0600

    seconOne

--- src/main/java/edu/miu/cs544/courseregistrationsystem/model/Student.java
+++ src/main/java/edu/miu/cs544/courseregistrationsystem/model/Student.java
@@ -1,2 +1,16 @@
-package edu.miu.cs544.courseregistrationsystem.model;public class Student {
+package edu.miu.cs544.courseregistrationsystem.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Student {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
 }
