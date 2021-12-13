e6f3126b737411477d93a7b7341a2f231d7b0986
Girmay-1 <54249189+Girmay-1@users.noreply.github.com>
2021-12-12 22:53:02 -0600

    started our project

---
+++ src/main/java/edu/miu/cs544/courseregistrationsystem/CourseRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.courseregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseRegistrationSystemApplication.class, args);
+    }
+
+}
