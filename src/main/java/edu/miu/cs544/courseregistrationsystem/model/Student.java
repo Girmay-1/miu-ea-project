a1042f5d0e766b1a76112acc8a1db93e4f27e709
bilguun1324 <b.bilguun1020@gmail.com>
2021-12-14 01:22:25 -0600

    fix mapping

--- src/main/java/edu/miu/cs544/courseregistrationsystem/model/Student.java
+++ src/main/java/edu/miu/cs544/courseregistrationsystem/model/Student.java
@@ -2,8 +2,11 @@ package edu.miu.cs544.courseregistrationsystem.model;
 
 import lombok.Data;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.Column;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
@@ -14,9 +17,23 @@ import java.util.List;
 @Data
 public class Student extends Person{
     private String studentId;
-//    @Embedded
-//    private Address mailingAddress;
     @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name="street",column=@Column(name="mailing_street")),
+        @AttributeOverride(name="city",column=@Column(name="mailing_city")),
+        @AttributeOverride(name="postalCode",column=@Column(name="mailing_postalCode")),
+        @AttributeOverride(name="stateOrProvince",column=@Column(name="mailing_stateOrProvince")),
+        @AttributeOverride(name="countryRegion",column=@Column(name="mailing_countryRegion"))
+     })
+    private Address mailingAddress;
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name="street",column=@Column(name="home_street")),
+        @AttributeOverride(name="city",column=@Column(name="home_city")),
+        @AttributeOverride(name="postalCode",column=@Column(name="home_postalCode")),
+        @AttributeOverride(name="stateOrProvince",column=@Column(name="home_stateOrProvince")),
+        @AttributeOverride(name="countryRegion",column=@Column(name="home_countryRegion"))
+     })
     private Address homeAddress;
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "student")
     List<Registration> registrations = new ArrayList<>();
